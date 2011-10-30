3de8c9fb941931c5eb0262f0efccf173509d28f3
rainyjune@live.cn <rainyjune@live.cn@834a0fc1-bcb4-e635-47aa-c9851844b3ba>
2011-10-30 02:05:32 +0000

    格式化代码

--- trunk/includes/install.php
+++ trunk/includes/install.php
@@ -65,7 +65,6 @@ if(!defined('IN_MP')){die('Access denied!');}
 		</script>
 	</head>
 	<body>
-
 		<div id="custom-doc" class="yui-t7">
 		<?php
 			if($installed){
@@ -95,51 +94,51 @@ if(!defined('IN_MP')){die('Access denied!');}
 					?>
 				<form action="index.php?action=install&l=<?php echo $language;?>" method="post">
 				<table align="center">
-				<tr>
-				<td><?php echo t('DB_TYPE',array(),$language); ?><span class="require">*</span></td>
-				<td>
-				<select id="dbtype" name="dbtype">
-				<option value=""><?php echo t('DB_TYPE_SELECT',array(),$language);?></option>
-				<optgroup label="FlatFile db" >
-				<option value="flatfile">Text DB API</option>
-				</optgroup>
-				<?php $db_types=get_supported_rdbms();if($db_types):?>
-				<optgroup label="RDBMS"> <!-- -->
-				<?php foreach ($db_types as $key => $value):?>
-				<option value="<?php echo $value;?>"><?php echo $key;?></option>
-				<?php endforeach;?><!-- -->
-				</optgroup>
-				<?php endif;?>
-				</select>
-				</td><td>&nbsp;</td>
-				</tr>
-				<tr>
-				<td><?php echo t('DB_HOST',array(),$language); ?><span class="require">*</span></td><td><input type="text" name="dbhost" value="localhost" /></td><td>&nbsp;</td>
-				</tr>
-				<tr>
-				<td><?php echo t('DB_USER',array(),$language); ?><span class="require">*</span></td><td><input type="text" name="dbusername" maxlength="10" value="root" /></td><td>&nbsp;</td>
-				</tr>
-				<tr>
-				<td><?php echo t('DB_PWD',array(),$language); ?></td><td><input type="text" name="dbpwd" maxlength="10" value="" /></td><td>&nbsp;</td>
-				</tr>
-				<tr>
-				<td><?php echo t('DB_NAME',array(),$language); ?><span class="require">*</span></td><td><input type="text" name="dbname" maxlength="10" value="mapleleaf" /></td><td>&nbsp;</td>
-				</tr>
-				<tr>
-				<td><?php echo t('TB_PREFIX',array(),$language); ?></td><td><input type="text" name="tbprefix" maxlength="10" value="mp_" /></td><td>&nbsp;</td>
-				</tr>
-				<tr>
-				<td><?php echo t('ADMIN_USERNAME',array(),$language); ?> <span class="require">*</span></td><td><input type="text" name="adminname" />&nbsp;</td><td><?php echo t('ADMIN_USERNAME_MIN', array(), $language);?></td>
-				</tr>
-				<tr>
-				<td><?php echo t('ADMIN_PASSWORD',array(),$language); ?><span class="require">*</span></td><td><input type="password" name="adminpass" /></td><td>&nbsp;</td>
-				</tr>
-				<tr>
-				<td colspan="3"><input type="checkbox" name="agree" id="agree" />&nbsp;<?php echo t('INSTALL_AGREEMENT', array(), $language);?></td>
-				</tr>
-				<tr>
-				<td colspan="3"><input id="submitButton" type="submit" value="<?php echo t('INSTALL', array(), $language);?>" /></td>
-				</tr>
+					<tr>
+						<td><?php echo t('DB_TYPE',array(),$language); ?><span class="require">*</span></td>
+						<td>
+							<select id="dbtype" name="dbtype">
+							<option value=""><?php echo t('DB_TYPE_SELECT',array(),$language);?></option>
+							<optgroup label="FlatFile db" >
+							<option value="flatfile">Text DB API</option>
+							</optgroup>
+							<?php $db_types=get_supported_rdbms();if($db_types):?>
+							<optgroup label="RDBMS"> <!-- -->
+							<?php foreach ($db_types as $key => $value):?>
+							<option value="<?php echo $value;?>"><?php echo $key;?></option>
+							<?php endforeach;?><!-- -->
+							</optgroup>
+							<?php endif;?>
+							</select>
+						</td><td>&nbsp;</td>
+					</tr>
+					<tr>
+						<td><?php echo t('DB_HOST',array(),$language); ?><span class="require">*</span></td><td><input type="text" name="dbhost" value="localhost" /></td><td>&nbsp;</td>
+					</tr>
+					<tr>
+						<td><?php echo t('DB_USER',array(),$language); ?><span class="require">*</span></td><td><input type="text" name="dbusername" maxlength="10" value="root" /></td><td>&nbsp;</td>
+					</tr>
+					<tr>
+						<td><?php echo t('DB_PWD',array(),$language); ?></td><td><input type="text" name="dbpwd" maxlength="10" value="" /></td><td>&nbsp;</td>
+					</tr>
+					<tr>
+						<td><?php echo t('DB_NAME',array(),$language); ?><span class="require">*</span></td><td><input type="text" name="dbname" maxlength="10" value="mapleleaf" /></td><td>&nbsp;</td>
+					</tr>
+					<tr>
+						<td><?php echo t('TB_PREFIX',array(),$language); ?></td><td><input type="text" name="tbprefix" maxlength="10" value="mp_" /></td><td>&nbsp;</td>
+					</tr>
+					<tr>
+						<td><?php echo t('ADMIN_USERNAME',array(),$language); ?> <span class="require">*</span></td><td><input type="text" name="adminname" />&nbsp;</td><td><?php echo t('ADMIN_USERNAME_MIN', array(), $language);?></td>
+					</tr>
+					<tr>
+						<td><?php echo t('ADMIN_PASSWORD',array(),$language); ?><span class="require">*</span></td><td><input type="password" name="adminpass" /></td><td>&nbsp;</td>
+					</tr>
+					<tr>
+						<td colspan="3"><input type="checkbox" name="agree" id="agree" />&nbsp;<?php echo t('INSTALL_AGREEMENT', array(), $language);?></td>
+					</tr>
+					<tr>
+						<td colspan="3"><input id="submitButton" type="submit" value="<?php echo t('INSTALL', array(), $language);?>" /></td>
+					</tr>
 				</table>
 				</form>
 				<?php
