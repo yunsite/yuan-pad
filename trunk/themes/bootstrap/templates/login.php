53cbf17dae67dac7a2e6a5e06a6ba6504ed019b4
rainyjune@live.cn <rainyjune@live.cn@834a0fc1-bcb4-e635-47aa-c9851844b3ba>
2014-06-05 05:54:22 +0000

    Update the bootstrap theme.

--- trunk/themes/bootstrap/templates/login.php
+++ trunk/themes/bootstrap/templates/login.php
@@ -1,44 +1,52 @@
 <?php if(!defined('IN_MP')){die('Access denied!');} ?>
 <!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta charset="utf-8" />
-<meta http-equiv="pragma" content="no-cache" />
-<meta http-equiv="Cache-Control" content="no-cache,must-revalidate" />
-<meta http-equiv="expires" content="0" />
-<title><?php echo t('LOGIN');?></title>
-</head>
-<body>
-<div id="backtoindex"><a href="index.php" title="<?php echo t('WHERE_AM_I');?>">&larr; <?php echo t('BACK');?></a></div>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title><?php echo t('LOGIN');?></title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+    <!-- Bootstrap -->
+    <link href="<?php echo './themes/'.ZFramework::app()->theme.'/css/bootstrap.min.css';?>" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
     <div class="container">
-	<?php if(@$errormsg){?>
-	    <div id="login_error" class="error"><?php echo $errormsg;?><br /></div>
-	<?php } ?>
+      <div id="backtoindex"><a href="index.php" title="<?php echo t('WHERE_AM_I');?>">&larr; <?php echo t('BACK');?></a></div>
+      <?php if(@$errormsg){?>
+      <div id="login_error" class="bg-danger"><?php echo $errormsg;?><br /></div>
+      <?php } ?>
 
-	<div id="loginForm">
-            <form action="<?php echo $_SERVER['PHP_SELF'];?>?controller=user&amp;action=login" method="post">
-                <table>
-                    <tr>
-		    <td><label><?php echo t('USERNAME');?></label></td>
-                    <td><input type="text" name="user" id="user" size="20" /></td>
-		    </tr>
-		    <tr>
-                        <td><label><?php echo t('ADMIN_PWD');?></label></td>
-			<td><input type="password" id="password" name="password" size="20" /></td>
-		    </tr>
-                    <tr>
-		    <td colspan="2">
-		        <input id="submit_button" name="submit" type="submit" value="<?php echo t('SUBMIT');?>" />
-		    </td>
-                    </tr>
-                </table>
-	    </form>
-	</div>
-
+      <form class="form-horizontal" role="form" action="<?php echo $_SERVER['PHP_SELF'];?>?controller=user&amp;action=login" method="post">
+        <div class="form-group">
+          <label for="user" class="col-sm-2 control-label"><?php echo t('USERNAME');?></label>
+          <div class="col-sm-10">
+            <input type="text" name="user" id="user" class="form-control">
+          </div>
+        </div>
+        <div class="form-group">
+          <label for="password" class="col-sm-2 control-label"><?php echo t('ADMIN_PWD');?></label>
+          <div class="col-sm-10">
+            <input type="password" name="password" id="password" class="form-control">
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-10 col-sm-offset-2">
+            <button type="submit" class="btn btn-default"><?php echo t('SUBMIT');?></button>
+          </div>
+        </div>
+      </form>
+    
+      <footer>
+      Powered by YuanPad <?php echo MP_VERSION;?>  Copyright &copy;2008-2014
+      </footer>
     </div>
-    <div class="copyright">
-	    Powered by YuanPad <?php echo MP_VERSION;?>  Copyright &copy;2008-2014
-    </div>
+    
     <script>window.jQuery || document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"><\/script>')</script>
-</body>
+  </body>
 </html>
